375a91f4099bbcac57a7ba89f000e8dcf31b71f1
jsmith@conflexion.com <jsmith@conflexion.com>
2018-09-24 08:58:52 -0500

    Added save and comments.

--- code/myplot.py
+++ code/myplot.py
@@ -1,9 +1,16 @@
+# Set things up
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Sample numpy array (linearly spaced values)
 x = np.linspace(0, 10, 100)
 
+# Plot sin(x) and cos(x)
 plt.plot(x, np.sin(x))
 plt.plot(x, np.cos(x))
 
+# Save default plot to file
+plt.savefig('myplot.png')
+
+# Show in interactive mode
 plt.show()
