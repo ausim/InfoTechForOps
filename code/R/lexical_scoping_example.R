eddfdd2c2ad9479322602402b1398af1189d6ca5
Jeff Smith <jsmith@conflexion.com>
2018-10-01 14:32:25 -0500

    Initial entry into the repository

---
+++ code/R/lexical_scoping_example.R
@@ -0,0 +1,21 @@
+#
+# Lexical Scoping Example
+#
+
+# Note that a is defined inside the function, y is used,
+# but not defined or passed as an arguement, z is defined
+# and returned.
+my_fun <- function(x) {
+  a = "dog"
+  z = x+y
+  print(ls())
+  return(z)
+}
+
+
+# note that the a and Z variables defined in the function
+# aren't in the environment.
+y = 207
+b = my_fun(13)
+print(ls())
+
