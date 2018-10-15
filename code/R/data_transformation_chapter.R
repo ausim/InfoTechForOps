67601aa32eda3012c65ef7478581250d87c1c516
jsmith@conflexion.com <jsmith@conflexion.com>
2018-10-15 08:32:39 -0500

    Update code/R/data_transformation_chapter.R
    Added pipe-related material.

--- code/R/data_transformation_chapter.R
+++ code/R/data_transformation_chapter.R
@@ -142,13 +142,47 @@ s <- summarize(by_dest,
 )
 s <- arrange(s, desc(num))
 
+
 # piping
+
+by_dest <- group_by(flights, dest)
+delay <- summarise(by_dest,
+                   count = n(),
+                   dist = mean(distance, na.rm = TRUE),
+                   delay = mean(arr_delay, na.rm = TRUE)
+)
+delay <- filter(delay, count > 20, dest != "HNL")
+
+# It looks like delays increase with distance up to ~750 miles 
+# and then decrease. Maybe as flights get longer there's more 
+# ability to make up delays in the air?
+ggplot(data = delay, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+#> `geom_smooth()` using method = 'loess' and formula 'y ~ x'
+
+# The 'pipe' allows you to do this type of multi-step process
+# without storing the intermediate results;
+delays <- flights %>% 
+  group_by(dest) %>% 
+  summarise(
+    count = n(),
+    dist = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)
+  ) %>% 
+  filter(count > 20, dest != "HNL")
+
+
+# Suppose that you want to know the popular destinations and thier distances
+
 popular_dests <- flights %>%
   group_by(dest) %>%
   summarize(
     num = n(),
     dist = mean(distance, na.rm=TRUE)
   ) %>%
+  filter(num > 5000) %>%
+  mutate(tot = num * dist) %>%
   arrange(desc(num))
   
   
