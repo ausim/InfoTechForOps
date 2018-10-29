227db94d4a9a7e4e510f588ccc812f48380ed00a
Jeff Smith <jsmith@conflexion.com>
2018-10-29 10:43:41 -0500

    Initial entry into the repository.

---
+++ code/R/databases_and_mysql.R
@@ -0,0 +1,27 @@
+#
+# databases_and_mysql.R
+#
+library("tidyverse")
+library("RMySQL")
+
+# Passengers
+pass <- read_csv("..\\data\\passengers.csv")
+
+# Flights
+flights <- read_csv("..\\data\\flights.csv")
+
+
+
+# Connect to DB and get table list
+# Note that you need to make sure that the db, user, passwd match
+#   the ones that you used when you setup the database.  If you used
+#   my script, they will.
+con = dbConnect(MySQL(),dbname='insy6500', user='insy6500',password='insy6500', host = 'localhost')
+dbListTables(con)
+
+#create dataframe with the flights
+myquery <-"SELECT * from flight;"
+dfFlights <- dbGetQuery(con,myquery)
+head(dfFlights)
+
+
