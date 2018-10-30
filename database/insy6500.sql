c1bb603ca33d3cfc430125762e84a09d1b90f095
Jeff Smith <jsmith@conflexion.com>
2018-10-30 12:58:59 -0500

    Update database/insy6500.sql
    Added 'use insy6500' to the script.

--- database/insy6500.sql
+++ database/insy6500.sql
@@ -12,6 +12,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+use insy6500;
 --
 -- Table structure for table `Airplane`
 --
