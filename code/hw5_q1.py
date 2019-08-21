0ea0f7a0e13f008e78f604c80f828631c516daee
Jeff Smith <jsmith@conflexion.com>
2019-08-21 11:29:55 -0500

    Reconfigured the code subdirectory

--- code/hw5_q1.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-rvgs = np.random.normal(123.5, 27.50, 10000)
-print("Mean: {:.2f}, Std. Dev: {:.2f}".format(np.mean(rvgs), np.std(rvgs)))
-fig = plt.hist(rvgs)
-plt.show()
---
+++ code/py/hello_world_loop.py
@@ -0,0 +1,4 @@
+for i in range(20):
+	j = i + 1
+	print ("[{:,d}] Hello World!".format(j))
+
