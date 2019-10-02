5c1adb17ff4e7252b86abd6ad51c2ee84b4e3a10
Jeff Smith <jsmith@conflexion.com>
2019-10-02 07:02:56 -0500

    Updated for Fall 2019

--- code/py/myplot.py
+++ code/py/myplot.py
@@ -6,8 +6,10 @@ import numpy as np
 x = np.linspace(0, 10, 100)
 
 # Plot sin(x) and cos(x)
-plt.plot(x, np.sin(x))
-plt.plot(x, np.cos(x))
+plt.plot(x, np.sin(x), 'r-')
+plt.plot(x, np.cos(x), 'b-')
+plt.title("Sine and Cosine Functions")
+plt.axis([-1, 11, -1.5, 1.5])
 
 # Save default plot to file
 plt.savefig('myplot.png')
