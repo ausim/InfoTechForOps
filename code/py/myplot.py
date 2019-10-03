a9a90b0eeb3b1dced54e265b11ff4be38e3eb38f
Jeff Smith <jsmith@conflexion.com>
2019-10-03 09:42:36 -0500

    Update myplot.py
    Added comments.

--- code/py/myplot.py
+++ code/py/myplot.py
@@ -1,3 +1,8 @@
+#
+# myplot.py - Simple plot example using Matplotlib
+#
+# Based on an example from VanderPlas (004.00 - Introduction to Matplotlib.ipynb)
+#
 # Set things up
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +13,9 @@ x = np.linspace(0, 10, 100)
 # Plot sin(x) and cos(x)
 plt.plot(x, np.sin(x), 'r-')
 plt.plot(x, np.cos(x), 'b-')
+# Add a title
 plt.title("Sine and Cosine Functions")
+# Change the axes limits
 plt.axis([-1, 11, -1.5, 1.5])
 
 # Save default plot to file
