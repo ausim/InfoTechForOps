beeee257bb71425f34ecfa77383d9b899802fcbf
Jeff Smith <jsmith@conflexion.com>
2019-11-11 07:42:08 -0600

    Added manual query to R
    And updated the jupyter version.

--- code/R/databases_and_mysql.R
+++ code/R/databases_and_mysql.R
@@ -11,13 +11,17 @@ pass <- read_csv("..\\..\\data\\passengers.csv")
 flights <- read_csv("..\\..\\data\\flights.csv")
 
 
-
 # Connect to DB and get table list
 # Note that you need to make sure that the db, user, passwd match
 #   the ones that you used when you setup the database.  If you used
 #   my script, they will.
 con = dbConnect(MySQL(),dbname='insy6500', user='insy6500',password='insy6500', host = 'localhost')
 dbListTables(con)
+
+# manually show the tables
+myquery <- "show tables;"
+alltables <- as_tibble(dbGetQuery(con, myquery))
+
 
 #create dataframe with the flights
 myquery <-"SELECT * from flight;"
